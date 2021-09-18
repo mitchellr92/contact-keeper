474a0f0848459bae0adfb240d13a8f14d9d45d04
Mitchell Robles <mitchellr92@live.com>
2021-09-18 15:26:11 -0700

    onsubmit, onchange and add contact functions

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import ContactContext from "../../context/contact/contactContext";
 
 const ContactForm = () => {
+  const contactContext = useContext(ContactContext);
+
   const [contact, setContact] = useState({
     name: "",
     email: "",
@@ -10,10 +13,22 @@ const ContactForm = () => {
 
   const { name, email, phone, type } = contact;
 
-  
+  const onChange = e =>
+    setContact({ ...contact, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    contactContext.addContact(contact);
+    setContact({
+      name: "",
+      email: "",
+      phone: "",
+      type: "personal"
+    });
+  };
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h2 className="text-primary">Add Contact</h2>
       <input
         type="text"
