b0dd3bb0fca61f8cc95299116fc37b890921b0aa
Mitchell Robles <mitchellr92@live.com>
2021-09-23 11:14:19 -0700

    Alert component

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import {v4 as uuid} from "uuid";
 import AlertContext from "./AlertContext";
 import AlertReducer from "./AlertReducer";
 import { SET_ALERT, REMOVE_ALERT } from "../types";
@@ -11,7 +11,7 @@ const AlertState = props => {
 
   // Set Alert
   const setAlert = (msg, type, timeout = 5000) => {
-    const id = uuid.v4();
+    const id = uuid();
     dispatch({
       type: SET_ALERT,
       payload: { msg, type, id }
