984cf0f60c267db4c2958959bbfdc8e7abcc1150
Mitchell Robles <mitchellr92@live.com>
2021-09-24 14:27:29 -0700

    get contacts - currently running into error

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,32 +1,75 @@
-import React, { useContext, Fragment } from "react";
+import React, { useContext, Fragment, useEffect } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactItem from "./ContactItem";
+import Spinner from "../layout/Spinner";
 import ContactContext from "../../context/contact/contactContext";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts, filtered } = contactContext;
+  const { contacts, filtered, getContacts, loading } = contactContext;
+
+  // useEffect(() => {
+  //   getContacts();
+  //   // eslint-disable-next-line
+  // }, []);
+
+  console.log(getContacts)
+
+  useEffect(() => {
+    getContacts();
+  });
 
   if (contacts.length === 0) {
     return <h4>Please add a contact</h4>;
   }
 
+  console.log(contacts);
+
   return (
+    // <Fragment>
+    //   {contacts !== null && !loading ? (
+    //     <TransitionGroup>
+    //       {filtered !== null
+    //         ? filtered.map(contact => (
+    //             <CSSTransition
+    //               key={contact._id}
+    //               timeout={500}
+    //               classNames="item"
+    //             >
+    //               <ContactItem contact={contact} />
+    //             </CSSTransition>
+    //           ))
+    //         : contacts.map(contact => (
+    //             <CSSTransition
+    //               key={contact._id}
+    //               timeout={500}
+    //               classNames="item"
+    //             >
+    //               <ContactItem contact={contact} />
+    //             </CSSTransition>
+    //           ))}
+    //     </TransitionGroup>
+    //   ) : (
+    //     <Spinner />
+    //   )}
+    // </Fragment>
     <Fragment>
-      <TransitionGroup>
-        {filtered !== null
-          ? filtered.map(contact => (
-              <CSSTransition key={contact.id} timeout={500} classNames="item">
-                <ContactItem contact={contact} />
-              </CSSTransition>
-            ))
-          : contacts.map(contact => (
-              <CSSTransition key={contact.id} timeout={500} classNames="item">
+      {contacts !== null && !loading ? (
+        <TransitionGroup>
+          {contacts.map(contact => (
+              <CSSTransition
+                key={contact._id}
+                timeout={500}
+                classNames="item"
+              >
                 <ContactItem contact={contact} />
               </CSSTransition>
             ))}
-      </TransitionGroup>
+        </TransitionGroup>
+      ) : (
+          <Spinner />
+        )}
     </Fragment>
   );
 };
